d4d93815620fc9ce4de635eed8fdadb8c2f1da54
Ahmad Faiz <ach.faiz.k@gmail.com>
2023-07-29 11:55:09 +0700

    feat(db): add database schema

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -1,29 +1,29 @@
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcrypt';
+import { PrismaClient, Role } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
 async function main() {
-    const hashedPassword = await bcrypt.hash('admin', 10);
-    const superadmin = await prisma.user.upsert({
-        where: { email: 'admin@admin.com' },
-        update: {},
-        create: {
-            name: 'Admin',
-            email: 'admin@admin.com',
-            password: hashedPassword,
-            role: 'SUPERADMIN',
-        },
-    });
-    console.log({ superadmin });
+  const hashedPassword = await bcrypt.hash("admin123", 10);
+  const superadmin = await prisma.user.upsert({
+    where: { email: "admin@admin.com" },
+    update: {},
+    create: {
+      name: "Admin",
+      email: "admin@admin.com",
+      password: hashedPassword,
+      role: Role.SUPERADMIN,
+    },
+  });
+  console.log({ superadmin });
 }
 
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
